2f646f6bcc04f29ba7be6e2cf48873598e5337ce
fengchuiguo1994 <huang182@live.cn>
2019-11-07 10:14:52 +0800

    20191106

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # leetcode
 the python/C code to solve the leetcode problem
+
+### 1.两数之和
+利用字典记录该列表的元素和位置,然后判断 target-当前元素 存不存在就行,所以至多遍历一次就行,时间复杂度O(n),空间复杂度:列表本身T(n),转成字典2T(n),合计3T(n)即T(n)。<br/>
+python:use time 60 ms
--- python/1.两数之和.py
+++ python/1.两数之和.py
@@ -23,3 +23,13 @@ nums = [2, 7, 11, 15]
 target = 9
 print(towSum(nums,target))
 print("use time {0:.10f} s".format(time.time()-be))
+
+# 抄袭的最快代码:(边遍历边存储字典中边判断)
+class Solution:
+     def twoSum(self, nums, target):
+            d = {}
+            for i, num in enumerate(nums):
+                if target - num in d:
+                    # 如果目标值减去该num的值在d的键值对里，则返回d[num]=i中的i，如果不存在，则使d[num]=i
+                    return d[target - num], i
+                d[num] = i
