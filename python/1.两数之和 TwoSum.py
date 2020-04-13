637ad95e2e3ebec14a710cb9a1d88476f9ded017
fengchuiguo1994 <huang182@live.cn>
2020-04-13 14:40:25 +0800

    Update python/1.两数之和 TwoSum.py

--- python/1.两数之和 TwoSum.py
+++ python/1.两数之和 TwoSum.py
@@ -4,8 +4,6 @@ import random
 def method1(nums,target):
     for i in range(len(nums)-1):
         for j in range(i+1,len(nums)):
-            if nums[i] == nums[j]: # can be drop
-                continue
             if nums[i] + nums[j] == target:
                 print(nums[i],nums[j],target)
                 return [i,j]
@@ -22,7 +20,7 @@ def method2(nums,target):
 def method3(nums,target):
     mydict = {}
     for i,num in enumerate(nums):
-        if target-num in mydict:
+        if target-num in mydict and mydict[target-num] != i:
             print(nums[mydict[target-num]],nums[i],target)
             return [mydict[target-num],i]
         mydict[num] = i
